5683a2dbd87cce3db29dbc50fb25e55458b91c44
Montse <zaratemontserrat74@gmail.com>
2026-03-11 23:01:09 -0600

    news

--- app/src/main/java/com/pajasoft/news_app/Components/NewsCard.kt
+++ app/src/main/java/com/pajasoft/news_app/Components/NewsCard.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.pajasoft.news_app.Models.Noticia
+import com.pajasoft.news_app.models.Noticia
 
 @Composable
 fun NewsCard(noticia: Noticia){
