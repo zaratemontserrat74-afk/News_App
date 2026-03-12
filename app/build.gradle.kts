43bbb4b85a02938116f4c686fb27bab7afb201b3
Montse <zaratemontserrat74@gmail.com>
2026-03-11 22:12:08 -0600

    elefante

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
