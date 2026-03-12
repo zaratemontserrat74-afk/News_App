c0765280f92835cd5eaaec05a4f5a9253ebb72e4
Montse <zaratemontserrat74@gmail.com>
2026-03-11 22:00:44 -0600

    Main

--- app/src/main/java/com/pajasoft/news_app/MainActivity.kt
+++ app/src/main/java/com/pajasoft/news_app/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pajasoft.news_app.ui.theme.News_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,15 +25,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             News_AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    newsApp(innerPadding)
                 }
             }
         }
     }
 }
+
+@Composable
+fun newsApp(innerPadding: PaddingValues){
+    val colors= MaterialTheme.colorScheme
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colors.background)
+            .padding(paddingValues = innerPadding)
+            .padding(horizontal = 15.dp)
+    ) {
+        Buscador()
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -42,6 +60,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     News_AppTheme {
-        Greeting("Android")
+        newsApp(innerPadding = PaddingValues(0.dp))
     }
 }
