69955edf6dc1fd903dc4287b5a653e3f672107bd
Montse <zaratemontserrat74@gmail.com>
2026-03-11 22:32:56 -0600

    cardimh

---
+++ app/src/main/java/com/pajasoft/news_app/Components/Cardimg.kt
@@ -0,0 +1,42 @@
+package com.pajasoft.news_app.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
+@Composable
+fun Cardimg(){
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(Color(0xFFF0F0F0))
+    ) {
+        AsyncImage(
+            model = noticia.img,
+            contentDescription = noticia.tittle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(125.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = noticia.title,
+            fontSize = 15.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
+}
